845205bdb9dff2564f109ac4b7123e2d5b03b592
Franklin <frankkiogora@yahoo.com>
2019-07-17 01:27:48 -0700

    updated settings file added Realtor app debugged app and made sure its running file at this point"

---
+++ listings/choices.py
@@ -0,0 +1,78 @@
+# Bed room choices
+
+
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10
+}
+
+
+# Price choices
+
+price_choices = {
+    '100000': '$100,000',
+    '200000': '$200,000',
+    '300000': '$300,000',
+    '400000': '$400,000',
+    '500000': '$500,000',
+    '600000': '$600,000',
+    '700000': '$700,000',
+    '800000': '$800,000',
+    '900000': '$900,000',
+    '1000000': '$1M+',
+}
+# county choice
+
+county_choices = {
+    'BA': 'Baringo',
+
+    'BO': 'Bomet',
+    'BS': 'Busia',
+    'EM': 'Elgeyo Marakwet',
+    'EB': 'Embu',
+    'GA': 'Garissa',
+    'HB': 'Homa Bay',
+    'IS': 'Isiolo',
+    'KJ': 'Kajiado',
+    'KK': 'Kakamega',
+    'KR': 'Kericho',
+    'KB': 'Kiambu',
+    'KF': 'Kilifi',
+    'KY': 'Kirinyaga',
+    'KI': 'Kisii',
+    'KT': 'Kitui',
+    'KW': 'Kwale',
+    'LK': 'Laikipia',
+    'LM': 'Lamu',
+    'MC':  'Machakos',
+    'MK': 'Makueni',
+    'ME':  'Meru',
+    'MG':  'Migori',
+    'MU':  "Murang'a",
+    'NB':  'Nairobi',
+    'NK':  'Nakuru',
+    'ND':  'Nandi',
+    'NR':  'Narok',
+    'NM':  'Nyamira',
+    'NN':  'Nyandarua',
+    'NI':  'Nyeri',
+    'SA':  'Samburu',
+    'SI':  'Siaya',
+    'TT':  'Taita Taveta',
+    'TR':  'Tana River',
+    'NT':  'Orissa',
+    'TN':  'Trans Nzoia',
+    'UG':  'Uasin Gishu',
+    'VI':  'Vihiga',
+    'WJ':  'Wajir',
+    'WP':  'West Pokot',
+
+}
