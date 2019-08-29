f346a51b212d4607000e697b98c2da3d11935592
Franklin <frankkiogora@yahoo.com>
2019-08-29 02:08:47 -0700

    changed settings file print function

--- vacationHomeFinder/settings.py
+++ vacationHomeFinder/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-print('DEBUG STATUS : 'DEBUG)
+print('DEBUG STATUS : ', DEBUG)
 
 ALLOWED_HOSTS = ['vacationhomefinder.herokuapp.com']
 
